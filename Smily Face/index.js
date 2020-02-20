8eedc87e71d617c7bff019c545c8302f9ffa9596
YUXUE LIU <47055928+liuyuxue97@users.noreply.github.com>
2020-02-20 18:15:38 +0100

    Add files via upload

---
+++ Smily Face/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { arc } from 'd3'
+
+const width = 960;
+const height = 500;
+const centerX = width/2;
+const centerY = height/2;
+const strokeWidth = 20;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 40;
+const mouthRadius=100;
+const mouthWidth=20
+
+const mouthArc=arc()
+    .innerRadius(mouthRadius)
+    .outerRadius(mouthRadius+mouthWidth)
+    .startAngle(Math.PI/2)
+    .endAngle(Math.PI*3/2);
+
+const App=()=>(
+  <svg width={width} height={height}>
+  <g transform={`translate(${centerX},${centerY}) `}>
+    <circle 
+      r={centerY-strokeWidth/2} 
+      fill="yellow"
+      stroke="black"
+      stroke-width={strokeWidth}
+    /> 
+    <circle 
+      cx={-eyeOffsetX} 
+      cy={-eyeOffsetY}
+      r={eyeRadius} 
+      fill="black"
+    />
+    <circle 
+      cx={eyeOffsetX} 
+      cy={-eyeOffsetY}
+      r={eyeRadius} 
+      fill="black"
+    />
+      <path d={mouthArc()}/>
+  </g>
+  </svg>
+);
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
